336db9859fb51674e28ca2c31d8041202593299b
Yevhenns <gifestum@gmail.com>
2025-03-26 18:09:28 +0200

    default config

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import dts from "vite-plugin-dts";
+// import { dirname, resolve } from "node:path";
+// import { fileURLToPath } from "node:url";
+// import dts from "vite-plugin-dts";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react(), dts()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "index.ts"),
-      name: "bme-calendar",
-      fileName: "index",
-    },
-    copyPublicDir: false,
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
+  plugins: [react()],
+  // build: {
+  //   lib: {
+  //     entry: resolve(__dirname, "index.ts"),
+  //     name: "bme-calendar",
+  //     fileName: "index",
+  //   },
+  //   copyPublicDir: false,
+  //   rollupOptions: {
+  //     external: ["react", "react-dom"],
+  //     output: {
+  //       globals: {
+  //         react: "React",
+  //         "react-dom": "ReactDOM",
+  //       },
+  //     },
+  //   },
+  // },
 });
