1e7c48c00e76c870ead1b5eb317cd3ef679bb60c
Yevhenns <gifestum@gmail.com>
2025-03-26 12:13:22 +0200

    added settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"), // вказуємо вхідний файл для бібліотеки
+      name: "BmeCalendar",
+      fileName: (format) => `bme-calendar.${format}.js`, // форматуємо назву файлу
+    },
+    rollupOptions: {
+      // Забезпечуємо експорт у форматах, сумісних з ES і CommonJS
+      external: ["react", "react-dom"], // не включаємо react в бандл
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
